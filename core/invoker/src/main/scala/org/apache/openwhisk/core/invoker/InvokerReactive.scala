848b0fc1b0c39e19c0e10c7fbc0063532a6fd32e
BillZong <billzong@163.com>
2019-11-11 13:26:25 +0800

    remove simple debug tests on slotsTooMuch message

--- core/invoker/src/main/scala/org/apache/openwhisk/core/invoker/InvokerReactive.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/invoker/InvokerReactive.scala
@@ -298,10 +298,11 @@ class InvokerReactive(
   private var resourceTooMuchCount = 0
   Scheduler.scheduleWaitAtMost(10.seconds)(() => {
     // TODO: 3 times check && node count determination
-    val msg = Metric("slotsTooMuch", 2) // remove 2 nodes
-    val resTopic = "resource"
-    healthProducer.send(resTopic, msg).andThen {
-      case Failure(t) => logging.warn(this, s"failed to send topic ($resTopic): $t")
-    }
+    logging.info(this, "TODO: 3 times check && node count determination")
+//    val msg = Metric("slotsTooMuch", 2) // remove 2 nodes
+//    val resTopic = "resource"
+//    healthProducer.send(resTopic, msg).andThen {
+//      case Failure(t) => logging.warn(this, s"failed to send topic ($resTopic): $t")
+//    }
   })
 }
